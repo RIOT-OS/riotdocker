a12b296f06cfddd11622791bc5c50d011a4271f9
Erik Stromdahl <erik.stromdahl@gmail.com>
2017-05-13 16:51:28 +0200

    scripts: add wrapper scripts for docker run and start
    docker-run.sh adds all necessary options for connecting
    usb devices (debug dongles) to the container.
    It also enables the user to launch GUI programs by letting
    programs inside the container connect to the xorg server
    of the host.
    
    docker-start.sh is just for convenience.
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

---
+++ docker-start.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# Fix for running GUI programs from within the docker container:
+# Make xorg disable access control, i.e. let any x client connect to our
+# server.
+xhost +
+
+docker start -ai riot
